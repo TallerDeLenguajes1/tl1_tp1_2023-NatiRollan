d23f15c1eaf0b6c45976c4036f1a7695aa59691d
“NatiRollan” <natyangela2010@gmail.com>
2023-03-26 00:33:58 -0300

    ejercicio 2.3b y 2.3e

--- tp1_1.c
+++ tp1_1.c
@@ -2,8 +2,18 @@
 
 int main()
 {
+    int a, *punt;
+
+    a= 5;
+    punt = &a;
     
     printf("Hola mundo");
+
+    printf("\nContenido del puntero: %d", *punt);
+    printf("\nDireccion de memoria almacenada por el puntero: %p", punt);
+    printf("\nDireccion de memoria de la variable: %p", &a);
+    printf("\nDireccion de memoria del puntero: %p", &punt);
+    printf("\nTamanio de memoria utilizada por la variable a: %d", sizeof(a));
 
     return 0;
 
